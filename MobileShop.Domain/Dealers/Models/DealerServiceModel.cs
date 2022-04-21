5b949808f493adf0b7c25bd0e0f5282e01ab1d35
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-04-22 00:02:36 +0300

    Add DealerServiceModels

---
+++ MobileShop.Domain/Dealers/Models/DealerServiceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MobileShop.Domain.Dealers.Models
+{
+    using MobileShop.Data.Entities;
+    public class DealerServiceModel
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
