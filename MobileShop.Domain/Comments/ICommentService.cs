a06ea31c91b3ccc36bdc8bb17c4d4bb8f81fad92
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-04-25 02:07:27 +0300

    Add ICommentService interface

---
+++ MobileShop.Domain/Comments/ICommentService.cs
@@ -0,0 +1,23 @@
+﻿namespace MobileShop.Domain.Comments
+{
+    using MobileShop.Domain.Comments.Models;
+    using System.Collections.Generic;
+    public interface ICommentService
+    {
+        public int Create(CommentServiceModel comment);
+
+
+        public bool Delete(CommentServiceModel comment);
+
+
+        public List<CommentServiceModel> FindAll();
+
+
+        public CommentServiceModel Read(int id);
+
+
+        public bool Update(CommentServiceModel comment);
+
+    
+    }
+}
