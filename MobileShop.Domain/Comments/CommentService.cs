914935b89af2cc59000a1b4e289fb1ed64a51430
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-04-25 21:19:49 +0300

    Implementing rest of methods in CommentService

--- MobileShop.Domain/Comments/CommentService.cs
+++ MobileShop.Domain/Comments/CommentService.cs
@@ -1,10 +1,13 @@
 ﻿namespace MobileShop.Domain.Comments
 {
+    using Microsoft.EntityFrameworkCore;
     using MobileShop.Data;
     using MobileShop.Data.Entities;
     using MobileShop.Domain.Comments.Models;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+
     public class CommentService : ICommentService
     {
         private readonly MobileShopDbContext data;
@@ -29,24 +32,77 @@
 
         }
 
-        public bool Delete(CommentServiceModel comment)
+        public bool Delete(CommentServiceModel entity)
         {
-            throw new System.NotImplementedException();
+            var comment = this.data.Comments.FirstOrDefault(c => c.Id == entity.Id);
+            if (comment != null)
+            {
+                this.data.Comments.Remove(comment);
+                this.data.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
         public List<CommentServiceModel> FindAll()
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var entities = this.data.Comments.AsNoTracking().ToList();
+                var comments = entities.Select(e => new CommentServiceModel
+                {
+                    Id = e.Id,
+                    CreatedOn = e.CreatedOn.ToString(),
+                    Description = e.Description,
+                    UserId = e.UserId,
+                    PhoneId = e.PhoneId
+                }).ToList();
+                return comments;
+            }
+            catch (Exception)
+            {
+                return new List<CommentServiceModel>();
+            }
         }
 
         public CommentServiceModel Read(int id)
         {
-            throw new System.NotImplementedException();
+            var entity = this.data.Comments.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            if (entity != null)
+            {
+                return new CommentServiceModel
+                {
+                    Id = entity.Id,
+                    CreatedOn = entity.CreatedOn.ToString(),
+                    Description = entity.Description,
+                    UserId = entity.UserId,
+                    PhoneId = entity.PhoneId
+                };
+            }
+            return null;
         }
 
-        public bool Update(CommentServiceModel comment)
+        public bool Update(CommentServiceModel entity)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var comment = new Comment
+                {
+                    Id = entity.Id,
+                    CreatedOn = DateTime.Parse(entity.CreatedOn),
+                    Description = entity.Description,
+                    UserId = entity.UserId,
+                    PhoneId = entity.PhoneId
+                };
+                this.data.Comments.Update(comment);
+                this.data.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
